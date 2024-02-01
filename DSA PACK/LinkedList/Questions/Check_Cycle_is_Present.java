c2d9d00ba86fa7a728fa61494cab6ba0afdf5708
Veerendra_Babu <veerendrababu80@gmail.com>
2024-02-01 13:18:34 +0530

    Check cycle is exists or Not in Linked List

---
+++ DSA PACK/LinkedList/Questions/Check_Cycle_is_Present.java
@@ -0,0 +1,23 @@
+
+// To slove this we use " Fast and slow pointers Method "
+// Fast = moves ahead by two steps
+// Slow = moves ahead by one step
+// whenever the fast and slow meet to each other the cycle is present
+
+public class Check_Cycle_is_Present {
+    public static void main(String[] args) {
+        
+    }
+    public static boolean hasCycle(MeargeTwoLinkedList head) {
+        MeargeTwoLinkedList fast = head;
+        MeargeTwoLinkedList slow = head;
+        while(fast != null && fast.next !=null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                return true;
+            }
+        }
+        return false;
+    }
+}
